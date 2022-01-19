8686d100ff7697fcc76826b41f92f52453e580eb
Дмитрий Мохирев <97874817+dvmohirev@users.noreply.github.com>
2022-01-20 01:05:16 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,28 @@ public class Main {
         int A = 0;
         int B = 0;
         char znak = ' ';
-        boolean isTrue = false;
+        boolean isWithoutWords = false;
         Scanner scanner = new Scanner(System.in);
-        while(!isTrue){
-            String line = scanner.nextLine();
 
-            String newLine = line.replaceAll("\\d", "").trim();
+        while(!isWithoutWords){
+            String line = scanner.nextLine();
+            String checkLine = line.replaceAll("([a-z])", "?").trim();
+            if(checkLine.contains("?")){
+                System.out.println("Ошибка! Введите выражение заново");
+            } else {
+                String newLine = line.replaceAll("\\d", "").trim();
+                znak = newLine.charAt(0);
+                A = Integer.parseInt(line.substring(0, line.indexOf(znak)));
+                B = Integer.parseInt(line.substring(line.indexOf(znak)+1));
+                isWithoutWords = true;
+            }
+        }
+
+        /*while(!isTrue){
+            String line = scanner.nextLine();
+            //String newLine = line.replaceAll("\\d", "").trim();
+            String newLine = line.replaceAll("([a-z])", "").trim();
+            System.out.println(newLine);
             znak = newLine.charAt(0);
             try{
                 A = Integer.parseInt(line.substring(0, line.indexOf(znak)));
@@ -24,7 +40,7 @@ public class Main {
             }catch (Exception e){
                 System.out.println("Ошибка! Введите выражение заново");
             }
-        }
+        }*/
 
         if (znak == plus){
             Calculate.sumNum(A, B);
